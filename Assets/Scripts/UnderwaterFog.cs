13d674918de3b90ec2aec7a62bb43c390786398a
emilyyyzh <emilyy.zhou@mail.utoronto.ca>
2024-01-13 13:49:59 -0500

    environment update

--- Assets/Scripts/UnderwaterFog.cs
+++ Assets/Scripts/UnderwaterFog.cs
@@ -1,20 +1,23 @@
- using UnityEngine;
- using System.Collections;
+using UnityEngine;
+using System.Collections;
  
- public class Underwater: MonoBehaviour {
+public class Underwater: MonoBehaviour {
  
- private Color underwaterColor;
+private Color underwaterColor;
+
+[SerializeField] Color fogColor = new Color(0.22f, 0.65f, 0.77f, 0.5f);
+    [SerializeField] float fogDensity = 0.002f;
+
+    // Use this for initialization
+    void Start () {
+    underwaterColor = fogColor;
+}
  
- // Use this for initialization
- void Start () {
-     underwaterColor = new Color (0.22f, 0.65f, 0.77f, 0.5f);
- }
- 
- // Update is called once per frame
- void Update () {
-     RenderSettings.fog = true;
-     RenderSettings.fogColor = underwaterColor;
-     RenderSettings.fogDensity = 0.01f;
- }
+// Update is called once per frame
+void Update () {
+    RenderSettings.fog = true;
+    RenderSettings.fogColor = underwaterColor;
+    RenderSettings.fogDensity = fogDensity;
+}
 
  }
